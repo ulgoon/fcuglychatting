ce36b48aa3dd58abfef5e0299782cb1055059187
ulgoon <ulgoon89@gmail.com>
2016-09-02 16:01:05 +0900

    broadcast

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 var express = require("express");
-
 var app = express();
 
-
 var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
 
 app.set("port", process.env.PORT || 3030);
 
 app.get("/", function(req, res){
-    res.type("text/plain");
-    res.send("This is chat server.");
+    res.sendFile(__dirname + "/views/index.html");
 });
+
+io.on("connection", function(socket){
+    socket.on("chat message", function(msg){
+        io.emit("chat message", msg);
+    });
+});
+
 
 http.listen(app.get("port"), function(){
     console.log("Express chat server is running at localhost:" + app.get("port"));
